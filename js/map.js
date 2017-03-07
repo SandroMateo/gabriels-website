498339807480617de2bcef83b002d472acd6b921
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2017-03-06 17:09:28 -0800

    Adds ability to remove data when radio button is clicked

--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   var map;
   function initMap() {
     var gabes = new google.maps.LatLng(45.5308236, -122.6444298);
@@ -7,9 +8,34 @@ $(document).ready(function(){
       center: gabes,
       zoom: 8
     });
-
-    map.data.loadGeoJson('../js/grocery.json');
-
   }
-  google.maps.event.addDomListener(window, 'load', initMap);
+
+  google.maps.event.addDomListener(window, 'load', function() {
+    initMap();
+
+    var groceryData = new google.maps.Data();
+    var marketData = new google.maps.Data();
+    var cafeData = new google.maps.Data();
+    groceryData.loadGeoJson('../js/grocery.json');
+    marketData.loadGeoJson('../js/markets.json');
+    cafeData.loadGeoJson('../js/cafes.json');
+    groceryData.setMap(map);
+    marketData.setMap(map);
+    cafeData.setMap(map);
+
+    $('#findByType').click(function() {
+      var locationType = $("input:radio[name=locationType]:checked").val();
+      console.log(locationType);
+      groceryData.setMap(null);
+      marketData.setMap(null);
+      cafeData.setMap(null);
+    });
+
+  });
+
+
+
+
+
+
 });
