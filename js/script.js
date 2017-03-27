7bd03a4b73de9e97056dfab11e748eb27b5f1f59
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2017-03-26 22:48:11 -0700

    Change sizing from em to rem and update styling for mobile nav to look closer to brand

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   $("table").stupidtable();
-  
+
+  // Need to update this code so that it will also toggle when people click outside of the mobile nav
+
   $(".mobile-nav__menu-button").on('touchstart click', function(){
     $(".mobile-nav__menu-wrapper").toggleClass("mobile-nav--display");
   });
