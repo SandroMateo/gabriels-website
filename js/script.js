0611c432daf6b2c917b5e0d4b52d7cb843f4fe9e
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2017-03-26 22:26:46 -0700

    Add HTML for mobile nav, initial styling for it, and mixins to make navbar responsive based on browser size

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
   $("table").stupidtable();
+  
+  $(".mobile-nav__menu-button").on('touchstart click', function(){
+    $(".mobile-nav__menu-wrapper").toggleClass("mobile-nav--display");
+  });
 });
