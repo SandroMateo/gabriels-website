c5b25b80b2782376bd964265277e2c07753b4b0a
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2017-03-27 11:44:19 -0700

    Update styling for mobile nav so the text is centered and replicates the feel of the sidebar menu items

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("table").stupidtable();
 
-  // Need to update this code so that it will also toggle when people click outside of the mobile nav
+  // Need to update this code so that it will also toggle when people click outside of the mobile nav. also if the menu is open and the screen gets bigger and then smaller again the menu is still open. so this whole function below should be rewritten. look for examples
 
   $(".mobile-nav__menu-button").on('touchstart click', function(){
     $(".mobile-nav__menu-wrapper").toggleClass("mobile-nav--display");
