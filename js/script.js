f8db423f28c629556a9415853463c6b16551de04
Sandro Alvarez <sandroalvarez@yahoo.com>
2017-03-30 21:32:49 -0700

    change youngGabriel image size and modify break points

--- js/script.js
+++ js/script.js
@@ -12,4 +12,6 @@ $(document).ready(function(){
   $(".mobile-nav__close-button").on('touchstart click', function(){
     $(".mobile-nav__menu-wrapper").removeClass("mobile-nav--display");
   });
+
+
 });
