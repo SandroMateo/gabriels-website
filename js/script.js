663976aa02017fe1f458578496bdb0501da48fdd
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2017-04-02 15:41:45 -0700

    Add temporary fixes to bread page and bagel page grid

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+  // To generate the nutrional fact table for bread anf bagels
   $("table").stupidtable();
 
   // Need to update this code so that it will also toggle when people click outside of the mobile nav. also if the menu is open and the screen gets bigger and then smaller again the menu is still open. so this whole function below should be rewritten. look for examples.
 
   // Now that I added the x button this is super janky in mobile view. probably has something to do with the touchstart part.
+
 
   $(".mobile-nav__menu-button").on('touchstart click', function(){
     $(".mobile-nav__menu-wrapper").addClass("mobile-nav--display");
@@ -13,6 +15,7 @@ $(document).ready(function(){
     $(".mobile-nav__menu-wrapper").removeClass("mobile-nav--display");
   });
 
+// This is for displaying/hiding the label for the contact form inputs
   $('.contact__item').on('change', function() {
     var input = $(this);
     if (input.val().length) {
