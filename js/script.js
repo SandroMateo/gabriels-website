67ff318217ee71cdbe29db87dfe68e568c2e13ab
Sandro Alvarez <sandroalvarez@yahoo.com>
2017-05-02 11:10:14 -0700

    try to fix mobile nav menu issues when hamburger menu is clicked

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
 $(document).ready(function(){
   // To generate the nutrional fact table for bread anf bagels
   $("table").stupidtable();
-  $("#myCarousel").carousel({
-    interval: false
-  });
-
-  // Need to update this code so that it will also toggle when people click outside of the mobile nav. also if the menu is open and the screen gets bigger and then smaller again the menu is still open. so this whole function below should be rewritten. look for examples.
-
-  // Now that I added the x button this is super janky in mobile view. probably has something to do with the touchstart part.
 
 
   $(".mobile-nav__menu-button").on('touchstart click', function(){
-    $(".mobile-nav__menu-wrapper").addClass("mobile-nav--display");
+    $(".mobile-nav__menu-button").hide();
+    $(".mobile-nav__menu-wrapper").slideDown("mobile-nav--display");
   });
 
   $(".mobile-nav__close-button").on('touchstart click', function(){
