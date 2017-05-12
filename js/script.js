f4e5825a63640900ff588ac730d1f2ac9fc34d28
Sandro Alvarez <sandroalvarez@yahoo.com>
2017-05-12 15:00:06 -0700

    start writing function for slideshow effect for featured images

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
-  // To generate the nutrional fact table for bread anf bagels
+
+  function startSlideshow(array, index, arrayLength) {
+    $(".new-item__wrapper").css("background-image", array[index])
+      // .fadeTo(3000, 1, function(
+      //   if (index < arrayLength) {
+      //     index++;
+      //   } else {
+      //     index = 0;
+      //   }
+      //   startSlideshow(array, index, arrayLength);
+      // ));
+  }
+  // To generate the nutritional fact table for bread anf bagels
   $("table").stupidtable();
+
+  var images = $(".new-item__wrapper").css("background-image");
+  var imageArray = images.split(",");
+  for (var i = 0; i < imageArray.length; i++) {
+    $(".new-item__wrapper").css("background-image", imageArray[i]);
+  }
+  // startSlideshow(imageArray, 0, imageArray.length);
+  console.log(imageArray);
 
 
   $(".mobile-nav__menu-button").on('touchstart click', function(e){
@@ -32,4 +52,6 @@ $(document).ready(function(){
   $('.sidebar__dropdown-button').mouseout(function(){
     $('.sidebar__dropdown').hide();
   });
+
+
 });
