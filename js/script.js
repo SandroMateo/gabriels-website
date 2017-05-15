1264d296884f6091af92f0cf01f36a0db264508f
Sandro Alvarez <sandroalvarez@yahoo.com>
2017-05-15 08:42:39 -0700

    try to fix slideshow function for featured pastries

--- js/script.js
+++ js/script.js
@@ -2,25 +2,20 @@ $(document).ready(function(){
 
   function startSlideshow(array, index, arrayLength) {
     $(".new-item__wrapper").css("background-image", array[index])
-      // .fadeTo(3000, 1, function(
-      //   if (index < arrayLength) {
-      //     index++;
-      //   } else {
-      //     index = 0;
-      //   }
-      //   startSlideshow(array, index, arrayLength);
-      // ));
+      if (index < arrayLength) {
+        index++;
+      } else {
+        index = 0;
+      }
+    setTimeout(startSlideshow(array, index, arrayLength), 5000);
   }
   // To generate the nutritional fact table for bread anf bagels
   $("table").stupidtable();
 
   var images = $(".new-item__wrapper").css("background-image");
   var imageArray = images.split(",");
-  for (var i = 0; i < imageArray.length; i++) {
-    $(".new-item__wrapper").css("background-image", imageArray[i]);
-  }
-  // startSlideshow(imageArray, 0, imageArray.length);
-  console.log(imageArray);
+  var imageLength = imageArray.length - 1;
+  startSlideshow(imageArray, 0, imageLength);
 
 
   $(".mobile-nav__menu-button").on('touchstart click', function(e){
